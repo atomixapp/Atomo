a441cfb2cf792983d54e112c133c312bab8d8538
atomixapp <silver1980sm@gmail.com>
2025-09-15 19:20:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-    const carousel = document.getElementById('carousel');
-    let startX, scrollLeft, isDown = false;
+// Carrusel plataformas
+const carousel = document.getElementById('carousel');
+let startX, scrollLeft, isDown = false;
 
-    carousel.addEventListener('mousedown', (e) => {
-      isDown = true;
-      startX = e.pageX - carousel.offsetLeft;
-      scrollLeft = carousel.scrollLeft;
-    });
+// Función para manejar el desplazamiento con el mouse
+carousel.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - carousel.offsetLeft;
+  scrollLeft = carousel.scrollLeft;
+});
 
-    carousel.addEventListener('mouseleave', () => {
-      isDown = false;
-    });
+carousel.addEventListener('mouseleave', () => {
+  isDown = false;
+});
 
-    carousel.addEventListener('mouseup', () => {
-      isDown = false;
-    });
+carousel.addEventListener('mouseup', () => {
+  isDown = false;
+});
 
-    carousel.addEventListener('mousemove', (e) => {
-      if (!isDown) return;
-      e.preventDefault();
-      const x = e.pageX - carousel.offsetLeft;
-      const walk = (x - startX) * 2; // Desplazamiento más rápido
-      carousel.scrollLeft = scrollLeft - walk;
-    });
+carousel.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - carousel.offsetLeft;
+  const walk = (x - startX) * 2; // Desplazamiento más rápido
+  carousel.scrollLeft = scrollLeft - walk;
+});
 
-    // Funcionalidad de deslizar con el dedo (Touch Events)
-    let touchStartX = 0;
-    let touchEndX = 0;
+// Funcionalidad de deslizar con el dedo (Touch Events)
+let touchStartX = 0;
+let touchEndX = 0;
 
-    carousel.addEventListener('touchstart', (e) => {
-      touchStartX = e.touches[0].clientX;
-    });
+carousel.addEventListener('touchstart', (e) => {
+  touchStartX = e.touches[0].clientX;
+});
 
-    carousel.addEventListener('touchmove', (e) => {
-      touchEndX = e.touches[0].clientX;
-    });
+carousel.addEventListener('touchmove', (e) => {
+  touchEndX = e.touches[0].clientX;
+});
 
-    carousel.addEventListener('touchend', () => {
-      if (touchStartX - touchEndX > 100) {
-        carousel.scrollLeft += 150; // Desplazamiento hacia la derecha
-      } else if (touchEndX - touchStartX > 100) {
-        carousel.scrollLeft -= 150; // Desplazamiento hacia la izquierda
-      }
-    });
+carousel.addEventListener('touchend', () => {
+  if (touchStartX - touchEndX > 100) {
+    carousel.scrollLeft += 150; // Desplazamiento hacia la derecha
+  } else if (touchEndX - touchStartX > 100) {
+    carousel.scrollLeft -= 150; // Desplazamiento hacia la izquierda
+  }
+});
