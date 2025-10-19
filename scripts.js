0a5203f00bfbf739469c5ab23483c8d40bbec3c6
atomixapp <silver1980sm@gmail.com>
2025-10-19 20:57:41 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
 let currentIndex = 0;
-let items = document.querySelectorAll('.carousel-item');
+const items = document.querySelectorAll('.carousel-item');
 
 function updateCarousel() {
     items.forEach((item, index) => {
-        item.classList.remove('center');
+        item.classList.remove('center', 'left', 'right');
         if (index === currentIndex) {
             item.classList.add('center');
+        } else if (index === currentIndex - 1 || (currentIndex === 0 && index === items.length - 1)) {
+            item.classList.add('left');
+        } else if (index === currentIndex + 1 || (currentIndex === items.length - 1 && index === 0)) {
+            item.classList.add('right');
         }
     });
 }
